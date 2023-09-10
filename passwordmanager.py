ee4d9b04689279432476e81bba11af4d255107ff
TN3W <141021574+tn3w@users.noreply.github.com>
2023-09-10 18:08:02 +0000

    Update passwordmanager.py

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,6 +1,6 @@
 import os
 import re
-logo = """ _____ _ _ _ ____     _____                         
+LOGO = """ _____ _ _ _ ____     _____                         
 |  _  | | | |    \   |     |___ ___ ___ ___ ___ ___ 
 |   __| | | |  |  |  | | | | .'|   | .'| . | -_|  _|
 |__|  |_____|____/   |_|_|_|__,|_|_|__,|_  |___|_|  
@@ -8,17 +8,19 @@ logo = """ _____ _ _ _ ____     _____
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(logo)
+    print(LOGO)
     password_file = input("Path to the password file or Enter to create a new one: ")
     if password_file == "":
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print(logo)
+            print(LOGO)
             print("Path to the password file or Enter to create a new one: \n")
             password_file_name = input("What should the password file be called: ")
             if re.search(r'[^a-zA-Z]', password_file_name):
                 print("[Error] File name can contain only letters.")
             else:
+                if password_file_name == "":
+                    password_file_name = "passwords"
                 break
         i = 0
         password_file = os.path.join(os.getcwd(), password_file_name + ("" if i == 0 else str(i)) + ".pwd")
